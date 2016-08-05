682fe2453e47ad01a70e6c9f2fa39014f8554b8d
Chandra Lindy <chandra.lindy@gmail.com>
2016-08-05 12:46:23 -0700

    refactor input.jsx

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -21642,57 +21642,34 @@
 	  value: true
 	});
 
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 	var _react = __webpack_require__(1);
 
 	var _react2 = _interopRequireDefault(_react);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var Input = function (_Component) {
-	  _inherits(Input, _Component);
-
-	  function Input() {
-	    _classCallCheck(this, Input);
-
-	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Input).apply(this, arguments));
-	  }
-
-	  _createClass(Input, [{
-	    key: "render",
-	    value: function render() {
-	      return _react2.default.createElement(
-	        "div",
-	        null,
-	        _react2.default.createElement(
-	          "h1",
-	          null,
-	          "CNA Assign ..."
-	        ),
-	        _react2.default.createElement(
-	          "div",
-	          { id: "maininput" },
-	          _react2.default.createElement("input", {
-	            type: "text",
-	            placeholder: "Enter commands here ...",
-	            onKeyDown: this.props.enter,
-	            autoFocus: true,
-	            id: "main"
-	          })
-	        )
-	      );
-	    }
-	  }]);
-
-	  return Input;
-	}(_react.Component);
+	var Input = function Input(props) {
+	  return _react2.default.createElement(
+	    "div",
+	    null,
+	    _react2.default.createElement(
+	      "h1",
+	      null,
+	      "CNA Assign ..."
+	    ),
+	    _react2.default.createElement(
+	      "div",
+	      { id: "maininput" },
+	      _react2.default.createElement("input", {
+	        type: "text",
+	        placeholder: "Enter commands here ...",
+	        onKeyDown: props.enter,
+	        autoFocus: true,
+	        id: "main"
+	      })
+	    )
+	  );
+	};
 
 	exports.default = Input;
 
--- src/input.jsx
+++ src/input.jsx
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 
-export default class Input extends Component {
+const Input = (props) => (
+  <div>
+    <h1>CNA Assign ...</h1>
+    <div id="maininput">
+      <input
+        type="text"
+        placeholder="Enter commands here ..."
+        onKeyDown={props.enter}
+        autoFocus={true}
+        id="main"
+      />
+    </div>
+  </div>
+);
 
-  render() {
-    return (
-      <div>
-        <h1>CNA Assign ...</h1>
-        <div id="maininput">
-          <input
-            type="text"
-            placeholder="Enter commands here ..."
-            onKeyDown={this.props.enter}
-            autoFocus={true}
-            id="main"
-          />
-        </div>
-      </div>
-    );
-  }
-}
+export default Input;
