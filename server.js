1f79d501efb8d98069881a2bbc3bca906c51255b
Chandra Lindy <chandra.lindy@gmail.com>
2016-10-14 11:09:22 -0700

    add bundle.js onto list

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/nurses', nurseCtrl.index);
 app.post('/nurse', nurseCtrl.post);
 app.post('/login', nurseCtrl.login);
 
-mongoose.connect('mongodb://localhost/db', () => {
+mongoose.connect('mongodb://heroku_5m0z7q6h:v758g3gdmrbv8su95hvjlckiuk@ds019826.mlab.com:19826/heroku_5m0z7q6h', () => {
   console.log('connected to mLab mongoDB');
 });
 
@@ -27,5 +27,3 @@ app.listen(process.env.PORT || 3000, function() {
 });
 
 module.exports = app;
-
-// mongodb://heroku_5m0z7q6h:v758g3gdmrbv8su95hvjlckiuk@ds019826.mlab.com:19826/heroku_5m0z7q6h
